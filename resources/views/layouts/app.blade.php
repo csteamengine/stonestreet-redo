c34c8121cb149fb8537b38e04503396866c6b5af
csteamengine <gcs@iastate.edu>
2017-12-21 01:21:19 -0600

    Added header for the home pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
     @yield('cssincludes')
 </head>
 <body>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,4 +6,19 @@
  * Time: 12:41 AM
  */
 ?>
-<h1>Header</h1>
+<header id="header">
+    <section id="homeSection">
+        <div id="Navbar">
+            <ul id="NavList">
+                <li id="Home"> <a href="/">Home</a></li>
+                <li id="Portfolio"><a href="/portfolio/">Portfolio</a></li>
+                <li><a href="//snippets.stonestreetsoftware.com">Snippets</a> </li>
+                <li id="About"><a href="/about/">About</a></li>
+                <li id="Contact"><a href="/contact/">Contact</a></li>
+            </ul>
+            <div id="logo">
+                <img src="/images/OneSLogo.png">
+            </div>
+        </div>
+    </section>
+</header>
