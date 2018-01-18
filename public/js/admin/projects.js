ef46f49ee4fafcf1761133fb77806a7ea8597bde
Gregory Steenhagen <gregorysteenhagen@Gregorys-MacBook-Pro.local>
2018-01-17 21:47:05 -0600

    Dont know what I changed

--- public/js/admin/projects.js
+++ public/js/admin/projects.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 13);
+/******/ 	return __webpack_require__(__webpack_require__.s = 15);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -10326,15 +10326,15 @@ return jQuery;
 
 /***/ }),
 
-/***/ 13:
+/***/ 15:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(14);
+module.exports = __webpack_require__(16);
 
 
 /***/ }),
 
-/***/ 14:
+/***/ 16:
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function($) {/* show file value after file select */
